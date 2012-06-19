6abb1afc9bc1aaec180ef69480646690c0260a1a
Bradley Morgan <morganconsulting@hotmail.com>
2012-06-19 15:18:49 -0500

    Initial Commit

---
+++ LDAPTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LDAPTest
+//
+//  Created by Bradley Morgan on 6/19/12.
+//  Copyright (c) 2012 Morgan Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
