ff750c63a4a9f1dc9399431135810c5103728c68
Bradley Morgan <morganconsulting@hotmail.com>
2012-06-20 16:43:31 -0500

    LDAP Experimenting

--- LDAPTest/main.m
+++ LDAPTest/main.m
@@ -7,16 +7,68 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <ldap.h>
+#import <lber.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+
+        char *ldapHost;
+        int ldapPort;
+        char *ldapUser;
+        char *ldapPass;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        printf("LDAP Host: ");
+        scanf("%s", &ldapHost);
         
-    }
+        // initialize ldap API
+        struct ldap *ld = ldap_open("adlb.auburn.edu", 389);
+        
+        // attempt a simple bind
+        int bindResult = ldap_simple_bind(ld, "checkdb01@auburn.edu", "webaccess");
+        
+        // attempt a search
+        
+        LDAPMessage *searchResult;
+        
+        ldap_search_ext_s(ld, "dc=auburn,dc=edu", LDAP_SCOPE_SUBTREE, "(objectclass=user)", NULL, 0, NULL, NULL, NULL, 20, &searchResult);
+        
+        LDAPMessage *searchEntries;
+        
+        for(searchEntries = ldap_first_entry(ld, searchResult); searchResult != NULL; searchResult = ldap_next_entry(ld, searchResult)) {
+            
+            char *a;
+            char **vals;
+            int i;
+            BerElement *ber;
+            
+            for ( a = ldap_first_attribute( ld, searchResult, &ber );
+                 
+                 a != NULL; a = ldap_next_attribute( ld, searchResult, ber ) ) {
+                
+                /* Get and print all values for each attribute. */
+                
+                if (( vals = ldap_get_values( ld, searchResult, a )) != NULL ) {
+                    
+                    for ( i = 0; vals[ i ] != NULL; i++ ) {
+                        
+                        printf( "%s: %s\n", a, vals[ i ] );
+                        
+                    }
+                    
+                }
+            
+            }
+        
+
+        }
+
     return 0;
+
+    }
+    
 }
+
 
